f9fcff87d22c1de651560bd48d41fc328d050d36
vanlouu <buildadiscord@gmail.com>
2024-12-04 12:39:07 -0500

    PlayerMovement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Movement();
+
+	}
+
+	public void Movement()
+	{
+		if(Input.GetKey(KeyCode.D))
+		{
+			gameObject.transform.position += new Vector3(.05f,0,0);
+		}
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(-.05f, 0, 0);
+        }
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(new Vector2(0,300));
+		}
+    }
+}
