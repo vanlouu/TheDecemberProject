f0b75eeba2c0c61788e14bd523892af279651161
arthur <steelstrange465@gmail.com>
2024-12-05 08:32:25 -0500

    Player jump good now

---
+++ Assets/Scripts/JumpCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpCheck : MonoBehaviour
+{
+	#region Variables
+	PlayerController PC;
+	#endregion
+	// Use this for initialization
+	void Start()
+	{
+		//get our player's controller
+		PC = GameObject.Find("Player").GetComponent<PlayerController>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		//check if we collided with the ground
+		if (other.tag == "Ground")
+		{
+			PC.Grounded = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+    {
+		//if we leave ground, we're no longer grounded (crazy I know)
+		if(other.tag == "Ground")
+        {
+			PC.Grounded = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    #region Variables
 
-	Rigidbody2D rb;
-	// Use this for initialization
-	void Start () {
+    #region Jump Vars
+    float JumpPower = 300; //amount of force we jump with
+    public   bool Grounded; //for checking if we should be able to jump
+
+    public float CoyoteTime = .2f; //amount of time after walking off a platform we cazzn still jump (makes it feel good)
+    private float CoyoteCounter;
+    public float JumpBuffer = .2f; //amount of time player can buffer the jump (in simple terms you can press space to jump a little before hitting the ground)
+    private float JumpBufferCounter;
+    #endregion
+
+    Rigidbody2D rb;
+    #endregion
+
+    // Use this for initialization
+    void Start () 
+	{
 		rb = GetComponent<Rigidbody2D>();	
 	}
 	
@@ -28,9 +42,35 @@ public class PlayerController : MonoBehaviour {
         {
             gameObject.transform.position += new Vector3(-.05f, 0, 0);
         }
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			rb.AddForce(new Vector2(0,300));
-		}
+
+        #region Jump Controls
+        //if we're on the ground, reset the coyote timer
+        if(Grounded)
+        {
+            CoyoteCounter = CoyoteTime;
+        }
+        //if we arent grounded, start counting
+        else
+        {
+            CoyoteCounter -= Time.deltaTime;
+        }
+
+        //when we press space, we reset the buffer
+        if(Input.GetKeyDown(KeyCode.Space))
+            JumpBufferCounter = JumpBuffer;
+        else
+            JumpBufferCounter -= Time.deltaTime;
+
+        //if player has pressed space (jump buffer) and we are still within coyote time, jump
+        if(JumpBufferCounter > 0f && CoyoteCounter > 0f)
+        {
+            //we're jumping so we can reset the buffer 
+            JumpBufferCounter = 0;
+            CoyoteCounter = 0;
+
+            //actually jumping
+            rb.AddForce(new Vector2(0, JumpPower));
+        }
+        #endregion
     }
 }
