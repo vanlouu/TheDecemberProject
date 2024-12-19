72b8b7dfe0ee67395ed6db6585411962bf4fdccd
vanlouu <buildadiscord@gmail.com>
2024-12-18 20:57:00 -0500

    finished areas/sounds/ and menus

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,12 +29,14 @@ public class PlayerController : MonoBehaviour {
     public PlatControls PlatCon;
     public PlatControls PlatCon2;
 
-    public GameObject dJump;
+    public GameObject dJump,StartMenu,WinMenu;
 
     Animator anim;
 
+    public bool Started=false;
+
     AudioSource aud;
-    public AudioClip NormalJump, DoubleJump, Death;
+    public AudioClip NormalJump, DoubleJump, Death, Button, Smoke;
     #endregion
 
     // Use this for initialization
@@ -58,97 +60,102 @@ public class PlayerController : MonoBehaviour {
 
 	public void Movement()
 	{
-        //moving right
-		if(Input.GetKey(KeyCode.D))
-		{
-			gameObject.transform.position += new Vector3(MoveSpeed*Time.deltaTime,0,0);
-            transform.localEulerAngles = new Vector3(0, 0, 0); //rotate the player to face the right direction
-            anim.SetBool("Moving", true);
-        }
 
-        //moving left
-        else if (Input.GetKey(KeyCode.A))
+        if (Started)
         {
-            gameObject.transform.position += new Vector3(-MoveSpeed * Time.deltaTime, 0, 0);
-            transform.localEulerAngles = new Vector3(0, 180, 0); //rotate the player to face the right direction
-            anim.SetBool("Moving", true);
-        }
+            //moving right
+            if (Input.GetKey(KeyCode.D))
+            {
+                gameObject.transform.position += new Vector3(MoveSpeed * Time.deltaTime, 0, 0);
+                transform.localEulerAngles = new Vector3(0, 0, 0); //rotate the player to face the right direction
+                anim.SetBool("Moving", true);
+            }
 
-        else
-        {
-            anim.SetBool("Moving", false);
-        }
+            //moving left
+            else if (Input.GetKey(KeyCode.A))
+            {
+                gameObject.transform.position += new Vector3(-MoveSpeed * Time.deltaTime, 0, 0);
+                transform.localEulerAngles = new Vector3(0, 180, 0); //rotate the player to face the right direction
+                anim.SetBool("Moving", true);
+            }
 
-        #region Jump Controls
-        //if we're on the ground, reset the coyote timer
-        if(Grounded)
-        {
-            CoyoteCounter = CoyoteTime;
-        }
-        //if we arent grounded, start counting
-        else
-        {
-            CoyoteCounter -= Time.deltaTime;
-        }
-
-        //when we press space, we reset the buffer
-        if(Input.GetKeyDown(KeyCode.Space))
-            JumpBufferCounter = JumpBuffer;
-        else
-            JumpBufferCounter -= Time.deltaTime;
-
-        //if player has pressed space (jump buffer) and we are still within coyote time, jump
-        if((JumpBufferCounter > 0f && CoyoteCounter > 0f) || HaveDoubleJump && Input.GetKeyDown(KeyCode.Space))
-        {
-           //if we're not grounded and jumping, it means we're double jumping 
-            if (!Grounded && CoyoteCounter <= 0)
-                HaveDoubleJump = false;
             else
             {
-                //we're jumping so we can reset the buffer 
-                JumpBufferCounter = 0;
-                CoyoteCounter = 0;
-                Grounded = false;
+                anim.SetBool("Moving", false);
             }
-            //actually jumping
-            if (HaveDoubleJump == false && CanDoubleJump) //if we have double jump ability and are using the double jump
+
+            #region Jump Controls
+            //if we're on the ground, reset the coyote timer
+            if (Grounded)
             {
-                rb.velocity = new Vector2(rb.velocity.x, JumpPower / 1.5f); //give weaker jump
-                Instantiate(dJump,GameObject.Find("JumpCheck").transform.position, Quaternion.identity);
-                aud.PlayOneShot(DoubleJump);
+                CoyoteCounter = CoyoteTime;
             }
+            //if we arent grounded, start counting
             else
             {
-                rb.velocity = new Vector2(rb.velocity.x, JumpPower); //otherwise jump normally
-           //     aud.PlayOneShot(NormalJump);
+                CoyoteCounter -= Time.deltaTime;
             }
+
+            //when we press space, we reset the buffer
+            if (Input.GetKeyDown(KeyCode.Space))
+                JumpBufferCounter = JumpBuffer;
+            else
+                JumpBufferCounter -= Time.deltaTime;
+
+            //if player has pressed space (jump buffer) and we are still within coyote time, jump
+            if ((JumpBufferCounter > 0f && CoyoteCounter > 0f) || HaveDoubleJump && Input.GetKeyDown(KeyCode.Space))
+            {
+                //if we're not grounded and jumping, it means we're double jumping 
+                if (!Grounded && CoyoteCounter <= 0)
+                    HaveDoubleJump = false;
+                else
+                {
+                    //we're jumping so we can reset the buffer 
+                    JumpBufferCounter = 0;
+                    CoyoteCounter = 0;
+                    Grounded = false;
+                }
+                //actually jumping
+                if (HaveDoubleJump == false && CanDoubleJump) //if we have double jump ability and are using the double jump
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, JumpPower / 1.5f); //give weaker jump
+                    Instantiate(dJump, GameObject.Find("JumpCheck").transform.position, Quaternion.identity);
+                    aud.PlayOneShot(DoubleJump);
+                }
+                else
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, JumpPower); //otherwise jump normally
+                                                                         //     aud.PlayOneShot(NormalJump);
+                }
+            }
+
+            //could have sworn i put this in earlier
+            //player fall faster
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMult - 1) * Time.deltaTime;
+            }
+
+            //player can short hop
+            else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMult - 1) * Time.deltaTime;
+                CoyoteCounter = 0f;
+            }
+
+            #endregion
+        }
         }
 
-        //could have sworn i put this in earlier
-        //player fall faster
-        if (rb.velocity.y < 0)
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMult - 1) * Time.deltaTime;
-        }
 
-        //player can short hop
-        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMult - 1) * Time.deltaTime;
-            CoyoteCounter = 0f;
-        }
-
-        #endregion
-    }
-
-
-    //trigger enter stuff
-   private void OnTriggerEnter2D(Collider2D other)
+        //trigger enter stuff
+        private void OnTriggerEnter2D(Collider2D other)
     {
         //fade away the black part
        if (other.tag=="Fade")
        {
            other.GetComponent<BoxCollider2D>().enabled = false; //so we dont reactivate the fading animation
+            aud.PlayOneShot(Smoke);
            StartCoroutine(Fade(other.gameObject));
        }
        //start the movement
@@ -163,13 +170,14 @@ public class PlayerController : MonoBehaviour {
             //call the platform moving method and give it the button num
             PlatCon.MovePlat(other.gameObject.GetComponent<ButtonInfo>().ButtonNum, other.gameObject.GetComponent<ButtonInfo>().Area, other.gameObject.GetComponent<ButtonInfo>().Removable, other.gameObject.GetComponent<ButtonInfo>().Destination, other.gameObject.GetComponent<ButtonInfo>().horozontal); other.GetComponent<BoxCollider2D>().enabled = false;
             other.GetComponent<SpriteRenderer>().enabled = false;
+            aud.PlayOneShot(Button);
         }
        //if you just want to remove an area (this might not be useful)
        if(other.tag=="TestRemoveButton")
         {
             PlatCon.RemovePlat(other.gameObject.GetComponent<ButtonInfo>().removeArea, other.gameObject.GetComponent<ButtonInfo>().removeDestination);
             other.gameObject.SetActive(false);
-               
+            aud.PlayOneShot(Button);
         }
        //if you want to remove an area and move one in
         if (other.tag == "BothButton")
@@ -177,6 +185,7 @@ public class PlayerController : MonoBehaviour {
             print("Huh?");
             PlatCon.MovePlat(other.gameObject.GetComponent<ButtonInfo>().ButtonNum, other.gameObject.GetComponent<ButtonInfo>().Area, other.gameObject.GetComponent<ButtonInfo>().Removable, other.gameObject.GetComponent<ButtonInfo>().Destination, other.gameObject.GetComponent<ButtonInfo>().horozontal); PlatCon.RemovePlat(other.gameObject.GetComponent<ButtonInfo>().removeArea, other.gameObject.GetComponent<ButtonInfo>().removeDestination);
             other.gameObject.SetActive(false);
+            aud.PlayOneShot(Button);
         }
         //move two areas at once (crazy i know)
         if (other.tag == "DoubleButton")
@@ -186,16 +195,30 @@ public class PlayerController : MonoBehaviour {
             PlatCon2.MovePlat(other.gameObject.GetComponent<ButtonInfo>().ButtonNum2, other.gameObject.GetComponent<ButtonInfo>().Area2, other.gameObject.GetComponent<ButtonInfo>().Removable2, other.gameObject.GetComponent<ButtonInfo>().Destination2, other.gameObject.GetComponent<ButtonInfo>().horozontal2);
             PlatCon2.RemovePlat(other.gameObject.GetComponent<ButtonInfo>().removeArea2, other.gameObject.GetComponent<ButtonInfo>().removeDestination2);
             other.gameObject.SetActive(false);
+            aud.PlayOneShot(Button);
         }
         
         //teleport player to the destination thats on the teleporter
         if(other.tag =="Kill")
         {
             gameObject.transform.position = other.gameObject.GetComponent<KillInfo>().tele.transform.position;
-       //     aud.PlayOneShot(Death);
+           aud.PlayOneShot(Death);
+        }
+        if(other.tag=="Win")
+        {
+            WinMenu.SetActive(true);
         }
 
    }
+
+
+
+
+    public void StartTheGame()
+    {
+        StartMenu.SetActive(false );
+        Started = true;
+    }
 
     //fade an object and then make it inactive
     IEnumerator Fade(GameObject obj)
